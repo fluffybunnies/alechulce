2dbe7e1cba013b30166ce223807b7ad627c99d1e
alec <alec.hulce@gmail.com>
2014-08-14 17:11:21 -0700

    u

--- web/HttpRules.php
+++ web/HttpRules.php
@@ -4,6 +4,7 @@ class HttpRules {
 
 	static function runIndexPages($pages=array('index.php','index.html','index.htm')){
 		foreach ($pages as $k => $v) {
+			echo "wfwef"
 			$check = $_SERVER['DOCUMENT_ROOT'].$_SERVER['DOCUMENT_URI'].$v;
 			if (file_exists($check)) {
 				include $check;
