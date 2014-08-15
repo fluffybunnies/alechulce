170d043fb9bac6feedf3aade5978f4959d921c36
alec <alec.hulce@gmail.com>
2014-08-14 18:10:32 -0700

    u

--- web/HttpRules.php
+++ web/HttpRules.php
@@ -4,11 +4,18 @@ class HttpRules {
 
 	static function run($rules=null){
 		if ($rules === null) {
-			$m = get_class_methods($this);
-			var_dump($m);
+			$rules = array();
+			foreach (get_class_methods('HttpRules') as $m) {
+				if (strpos($m,'run') === 0)
+					$rules[] $m;
+			}
 		}
 		if (!is_array($rules))
-			throw new Exception('');
+			throw new Exception('invalid rules');
+		foreach ($rules as $m) {
+			if (self::$m())
+				exit;
+		}
 	}
 
 	static function runIndexPages($pages=array('index.php','index.html','index.htm')){
@@ -20,6 +27,18 @@ class HttpRules {
 			}
 		}
 		return false;
+	}
+
+	static function run404($ignoreExtensions=array('ico,css,js,gif,jpg,jpeg,png')){
+		// ignoreExtensions so 404 resources dont set cookies, or attempt infinite redirect, etc
+		$ignoreExtensions = is_array($ignoreExtensions) ? array_fill_keys($ignoreExtensions, true) : array();
+		$ext = preg_match('/\/.+\.(.+)$/');
+		var_dump($ignoreExtensions);
+		var_dump($ext);
+		if ($ignoreExtensions && !empty($ignoreExtensions[$ext]))
+			return false;
+		header(isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0').' 404 Not Found');
+		return true;
 	}
 
 }
