af50e92d935d3859d39e1d7ad414a73d7d82b919
alec <alec.hulce@gmail.com>
2014-08-14 18:14:15 -0700

    u

--- web/HttpRules.php
+++ web/HttpRules.php
@@ -6,10 +6,10 @@ class HttpRules {
 		if ($rules === null) {
 			$rules = array();
 			$methods = get_class_methods('HttpRules');
-			foreach ($methods as $m) {
+			/*foreach ($methods as $m) {
 				if (strpos($m,'run') === 0)
 					$rules[] = $m;
-			}
+			}*/
 		}
 		if (!is_array($rules))
 			throw new Exception('invalid rules');
