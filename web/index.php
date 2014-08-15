22cea85b9336c0584e1d7a5ea8892f6e969bb42c
alec <alec.hulce@gmail.com>
2014-08-14 17:25:46 -0700

    u

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
 <?php
 
+define('WEBROOT', dirname(__FILE__));
+echo WEBROOT;
+
+include dirname(__FILE__).'/bootshell.php';
+
+exit;
+
 include 'HttpRules.php';
+
+HttpRules::run()
 
 HttpRules::runIndexPages();
